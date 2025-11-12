42c7c878d90cc7366d79ab058a6eed9e8e19314c
Fadhil Andriawan <fadhil@ilcs.co.id>
2025-11-12 15:45:44 +0700

    [UPDATE] page 1 & fix deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,31 @@
+<script>
+    import couplePic from '$lib/assets/images/couple-pic.jpg';
+</script>
+
 <main class="container">
-    Ini page utama
+    <section class="page1">
+        <div class="text-section">
+            <div class="flex justify-between">
+                <span>Your presence is requested</span>
+            </div>
+            <div>
+                <span>To celebrate</span>
+            </div>
+            <div>
+                <span>The wedding of</span>
+            </div>
+            <div class="flex justify-center py-5">
+                <span class="font-greatvibes! text-[2.5em]! text-accent font-normal! tracking-wider">Indri & Adhis</span>
+            </div>
+            <div class="flex justify-center">
+                <span>29.11.25</span>
+            </div>
+        </div>
+
+        <div class="image-section">
+            <div class="image-wrapper">
+                <img src={couplePic} alt="couplePic1"/>
+            </div>
+        </div>
+    </section>
 </main>
